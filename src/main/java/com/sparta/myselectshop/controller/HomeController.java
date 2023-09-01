d1977c60857086ce7e2eed175c956200cafee607
DESKTOP-HMN2L1J\정강용 <tuto3355@naver.com>
2023-09-01 16:32:25 +0900

    #로컬 저장소 옮긴 후 테스트

--- src/main/java/com/sparta/myselectshop/controller/HomeController.java
+++ src/main/java/com/sparta/myselectshop/controller/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class HomeController { //테스트용임시
     @GetMapping("/")
     public String home() {
         return "index";
